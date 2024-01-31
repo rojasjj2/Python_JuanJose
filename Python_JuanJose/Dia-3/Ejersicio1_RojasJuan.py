bfb62d8cb8062d7baf1816ad1f5ff4d85032def9
juan <rojasjjrp272@gmail.com>
2024-01-31 06:32:37 -0500

    ejercicio q no entendi

---
+++ Python_JuanJose/Dia-3/Ejersicio1_RojasJuan.py
@@ -0,0 +1,17 @@
+t = int(input())
+
+x = (input())
+nyk = [x]
+
+for i in range(2):
+    num=int(nyk[i])
+    nyk[i]=num
+
+y = (input())
+l = [y]
+
+for i2 in range(nyk[0]):
+    num2=int(l[i2])
+    l[i2]=num2
+
+def info (t)
